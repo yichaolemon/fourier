7039d3d0c1fcd1e579c8ca306ed3732ffd182ee8
yichaolemon <yclemontree0@gmail.com>
2022-06-12 17:27:41 -0700

    does a fft and can draw a sine wave

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.8-alpine
 
 WORKDIR /app
 
-RUN pip install Flask
+RUN pip install Flask && apk add py3-numpy && apk add g++ && pip install numpy
 
 CMD ["python3", "view.py"]
--- view.py
+++ view.py
@@ -1,19 +1,41 @@
+from random import sample
 from flask import Flask, render_template
 import math
 import os
+import numpy as np
+import cmath
 
 app = Flask(__name__)
 
+def transform(samples):
+    #t = np.fft.fft(10*np.cos(2 * np.pi * np.arange(sam) / 8) + 20j*np.sin(2*np.pi * np.arange(8) / 8)) / 
+    t = np.fft.fft(samples) / len(samples)
+    f = np.fft.fftfreq(len(samples))
+    zipped = sorted(zip(f, t), key=lambda x: abs(x[0]))
+    f = [x for (x, _) in zipped]
+    t = [x for (_, x) in zipped]
+    coefficients = list(abs(num) for num in t)
+    thetas = list(cmath.phase(num) for num in t)
+    print(f)
+    print(coefficients)
+    print(thetas)
+    return (coefficients, thetas, f)
+
+def samples(name):
+    if name == 'sine':
+        return 2*(30j*np.sin(2 * np.pi * np.arange(1000) / 1000) + np.arange(1000)/10 - 50)
+    elif name == 'circle':
+        return 100 * np.exp(2j * np.pi * np.arange(20) / 20)
+    return None
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/draw/<svg>')
-def draw_svg(svg: str):
-    coefficients = [200, 30, 10]
-    coefficients_theta = [math.pi/4, 1, 1]
-    return render_template('draw.html', svg_name=svg, coefficients_r=coefficients, coefficients_theta=coefficients_theta)
+@app.route('/draw/<name>')
+def draw_svg(name: str):
+    (coefficients, coefficients_theta, freq) = transform(samples(name))
+    return render_template('draw.html', svg_name=name, coefficients_r=coefficients, coefficients_theta=coefficients_theta, freq=freq)
 
 
 if __name__ == "__main__":
